2b820bf22e267335a3de0b78d2ddd76ee6e4ee0a
Brian1315 <jfhv1@alu.ua.es>
2025-12-01 11:44:06 +0100

    modularizando la adquisición de datos de kunna a la carpeta services (ahora no se obtienen los values en el response)

--- controllers/acquireController.js
+++ controllers/acquireController.js
@@ -1,5 +1,6 @@
 // controllers/acquireController.js
 const database = require('../services/database');
+const { getDataKunna } = require('../services/kunnaDataAcquisition');
 
 const health = (req, res) => {
     res.status(200).json({
@@ -8,84 +9,12 @@ const health = (req, res) => {
     });
 };
 
-const getDates = () => {
-    const now = new Date();
-
-    const options = { 
-        timeZone: 'Europe/Madrid', 
-        hour: '2-digit',       
-        hour12: false          
-    };
-    
-    const madridHourString = now.toLocaleString('es-ES', options);
-    const madridHour = parseInt(madridHourString);
-
-    let targetDate = new Date(now);
-
-    if (madridHour >= 23){
-        targetDate.setDate(now.getDate() + 1);
-    }
-    console.log(`[ACQUIRE DATE] La fecha objetivo es ${targetDate.toLocaleDateString()}`);
-
-    const endDate = new Date(targetDate);
-    endDate.setDate(targetDate.getDate() - 1);
-    const startDate = new Date(endDate);
-    startDate.setDate(endDate.getDate() - 3);
-
-    const startDateForApi = startDate.toISOString().split('.')[0] + 'Z';
-    const endDateForApi = endDate.toISOString().split('.')[0] + 'Z';
-
-    return {startDateForApi, endDateForApi};
-};
-
 // Como no se pasa body no sé cual va a ser la fecha objetivo, es decir,
 // si se va a poder pasar la fecha objetivo por parámetro para hacer la
 // obtención de datos de un día concreto que no sea para hoy o mañana 
-const data = async (req, res) => {
-    const { startDateForApi, endDateForApi } = getDates();
-
-    console.log(startDateForApi, endDateForApi);
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.KUNNA_API_KEY}`
-    }
-
-    body = {
-            "time_start": startDateForApi,
-            "time_end": endDateForApi,
-
-                "filters": [
-                {
-                "filter": "name",
-                "values": [
-                    "1d"
-                ]
-                }, {"filter": "uid",
-                "values": [
-                    "MLU00360002"
-                ]
-                }
-            ],
-
-            "limit": 100,
-            "count": false,
-            "order": "DESC"
-            }
-    
+const data = async (req, res) => {    
     try {
-        const response = await fetch(process.env.KUNNA_URL, {
-            method: 'POST',
-            headers: headers, 
-            body: JSON.stringify(body),
-        });
-
-        if(!response.ok) {
-            const errorBody = await response.text();
-            throw new Error(`${response.status} ${response.statusText} - ${errorBody}`);
-        }
-
-        const apiData = await response.json();
+        const apiData = getDataKunna();
         res.status(200).json({
             message: 'Datos obtenidos con exito',
             data: apiData
---
+++ services/kunnaDataAcquisition.js
@@ -0,0 +1,80 @@
+// services/kunnaDataAcquisition.js
+
+const getDates = () => {
+    const now = new Date();
+
+    const options = { 
+        timeZone: 'Europe/Madrid', 
+        hour: '2-digit',       
+        hour12: false          
+    };
+    
+    const madridHourString = now.toLocaleString('es-ES', options);
+    const madridHour = parseInt(madridHourString);
+
+    let targetDate = new Date(now);
+
+    if (madridHour >= 23){
+        targetDate.setDate(now.getDate() + 1);
+    }
+    console.log(`[ACQUIRE DATE] La fecha objetivo es ${targetDate.toLocaleDateString()}`);
+
+    const endDate = new Date(targetDate);
+    endDate.setDate(targetDate.getDate() - 1);
+    const startDate = new Date(endDate);
+    startDate.setDate(endDate.getDate() - 3);
+
+    const startDateForApi = startDate.toISOString().split('.')[0] + 'Z';
+    const endDateForApi = endDate.toISOString().split('.')[0] + 'Z';
+
+    return {startDateForApi, endDateForApi};
+};
+
+const getDataKunna = async () => {
+    const { startDateForApi, endDateForApi } = getDates();
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.KUNNA_API_KEY}`
+    }
+
+    body = {
+            "time_start": startDateForApi,
+            "time_end": endDateForApi,
+
+                "filters": [
+                {
+                "filter": "name",
+                "values": [
+                    "1d"
+                ]
+                }, {"filter": "uid",
+                "values": [
+                    "MLU00360002"
+                ]
+                }
+            ],
+
+            "limit": 100,
+            "count": false,
+            "order": "DESC"
+            }
+    
+    
+    const response = await fetch(process.env.KUNNA_URL, {
+        method: 'POST',
+        headers: headers, 
+        body: JSON.stringify(body),
+    });
+
+    if(!response.ok) {
+        const errorBody = await response.text();
+        throw new Error(`${response.status} ${response.statusText} - ${errorBody}`);
+    }
+
+    const apiData = await response.json();
+    console.log(apiData);
+    return apiData;
+};
+
+module.exports = { getDataKunna };
